ada2e8d03305e7440ab943e13e351badedd9ba50
Leonard Shtika <leoshtika@gmail.com>
2016-09-22 23:48:58 +0300

    Resolve #1: Create a basic structure with sidemenu

--- www/js/app.js
+++ www/js/app.js
@@ -1,73 +1,62 @@
-// Ionic Starter App
+// eventkitApp is the main application module
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var eventkitApp = angular.module('eventkitApp', ['ionic']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+eventkitApp.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+        .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html',
+            controller: 'appController'
+        })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+        .state('app.event-info', {
+            url: '/event-info',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/event-info.html'
+                }
+            }
+        })
+        
+        .state('app.speakers', {
+            url: '/speakers',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/speakers.html',
+                    controller: 'SpeakersController'
+                }
+            }
+        })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+        .state('app.speaker', {
+            url: '/speakers/:speakerId',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/speaker.html',
+                    controller: 'SpeakersController'
+                }
+            }
+        });
+        
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/event-info');
 });
---
+++ www/js/controllers/speakersController.js
@@ -0,0 +1,10 @@
+eventkitApp.controller('SpeakersController', function ($scope) {
+    $scope.speakers = [
+        {name: 'Leonard Shtika', id: 1},
+        {name: 'John Smith', id: 2},
+        {name: 'Ashlee Atienza ', id: 3},
+        {name: 'Fernando Flinchum ', id: 4},
+        {name: 'Kate Benigno', id: 5},
+        {name: 'Eulalia Reddin', id: 6}
+    ];
+});
