f623b966b49e6d53fbf43a35f21b542bd276a897
Leonard Shtika <leoshtika@gmail.com>
2016-10-10 00:33:27 +0300

    Change urlAPI to online API

--- www/js/app.js
+++ www/js/app.js
@@ -2,10 +2,10 @@
 
 var eventkitApp = angular.module('eventkitApp', ['ionic'])
     .constant('CONST', {
-//        urlAPI: 'http://localhost:4000/api/',
-//        urlFiles: 'http://localhost:4000/files4users/'
-        urlAPI: 'http://192.168.1.100:4000/api/',
-        urlFiles: 'http://192.168.1.100:4000/files4users/'
+        urlAPI: 'http://web-sapiens.net/eventkit/api/',
+        urlFiles: 'http://web-sapiens.net/eventkit/files4users/'
+//        urlAPI: 'http://192.168.1.100:4000/api/',
+//        urlFiles: 'http://192.168.1.100:4000/files4users/'
     });
 
 eventkitApp.run(function ($ionicPlatform, UserService) {
